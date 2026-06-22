801b0a5603e8384013ec7706a84a674ed4df2690
torrentspec <torrentpagla@gmail.com>
2026-06-22 07:35:45 +0530

    Fix: Swap Kotlin for Java Activity to ensure successful compilation

---
+++ app/src/main/java/com/torrentspec/madhu/MainActivity.java
@@ -0,0 +1,12 @@
+package com.torrentspec.madhu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Empty screen to prevent crashes
+    }
+}
