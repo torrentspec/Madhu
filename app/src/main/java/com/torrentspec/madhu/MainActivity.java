26feffc9ac08d2f61f74b05b96784217894495fc
torrentspec <torrentpagla@gmail.com>
2026-06-22 07:41:43 +0530

    Feat: Add Welcome UI and connect it to MainActivity

--- app/src/main/java/com/torrentspec/madhu/MainActivity.java
+++ app/src/main/java/com/torrentspec/madhu/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Empty screen to prevent crashes
+        // This line tells Android to draw the XML file we just created!
+        setContentView(R.layout.activity_main);
     }
 }
